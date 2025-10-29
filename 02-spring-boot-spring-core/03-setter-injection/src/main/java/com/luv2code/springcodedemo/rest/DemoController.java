ce400916ea4af474f54dbad7f8da3f2be362054d
Amr Kahla <amrkahla6@gmail.com>
2025-10-29 12:45:10 +0300

    setter-injection

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/luv2code/springcodedemo/rest/DemoController.java
@@ -0,0 +1,23 @@
+package com.luv2code.springcodedemo.rest;
+
+import com.luv2code.springcodedemo.common.CoachInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private CoachInterface myCoach;
+
+    @Autowired
+    public void setMyCoach(CoachInterface theCoach){
+        myCoach = theCoach;
+    }
+
+    @GetMapping("dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
