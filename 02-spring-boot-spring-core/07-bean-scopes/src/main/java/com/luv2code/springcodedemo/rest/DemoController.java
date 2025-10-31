d03fc45ff61038f34057e64f58b865975c19abbe
Amr Kahla <amrkahla6@gmail.com>
2025-10-31 11:45:29 +0200

    bean-scopes

---
+++ 02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/luv2code/springcodedemo/rest/DemoController.java
@@ -0,0 +1,35 @@
+package com.luv2code.springcodedemo.rest;
+
+import com.luv2code.springcodedemo.common.CoachInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private CoachInterface myCoach;
+    private CoachInterface anotherCoach;
+
+    @Autowired
+    public DemoController(
+            @Qualifier("cricketCoach") CoachInterface theCoach,
+            @Qualifier("cricketCoach") CoachInterface theAnotherCoach
+    ){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+        myCoach = theCoach;
+        anotherCoach = theAnotherCoach;
+    }
+
+    @GetMapping("dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check(){
+        return "Comparing beans: myCoach == anotherCoach, " + (myCoach == anotherCoach);
+    }
+}
