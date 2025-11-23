8af01a9be4cad06269a584f1b33537441e84e464
Amr Kahla <amrkahla6@gmail.com>
2025-11-23 18:39:04 +0200

    spring-boot-rest-crud-students-global-exception-handling

---
+++ 04-spring-boot-rest-crud/06-spring-boot-rest-crud-students-global-exception-handling/src/main/java/com/luv2demo/demo/rest/StudentRestController.java
@@ -0,0 +1,47 @@
+package com.luv2demo.demo.rest;
+
+import com.luv2demo.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> theStudents;
+
+    // define @PostConstruct to load the student data ... only once!
+    @PostConstruct
+    public void loadDate(){
+        theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("Amr","Kahla"));
+        theStudents.add(new Student("Abed","Kahla"));
+        theStudents.add(new Student("Eman","Abdelrahman"));
+    }
+
+    // define endpoint for "/students" - return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        return theStudents;
+    }
+
+    // define endpoint or "/students/{studentId}" - return student at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable  int studentId){
+
+        // just index into the list ... keep it simple for now
+
+        // check the studentId again list size
+        if ( (studentId >= theStudents.size()) || (studentId < 0) ){
+            throw new StudentNotFountException("Student id not found - " + studentId);
+        }
+
+        return theStudents.get(studentId);
+    }
+}
---
+++ 04-spring-boot-rest-crud/06-spring-boot-rest-crud-students-global-exception-handling/src/main/java/com/luv2demo/demo/rest/StudentRestExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.luv2demo.demo.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class StudentRestExceptionHandler {
+
+    // add exception handling code here
+
+    // Add an exception handler using @ExceptionHandler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFountException exe){
+        // create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exe.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exe){
+        // create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exe.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
