8055b6e8a55bffdad587fe256e8ef9b6e7b133db
Amr Kahla <amrkahla6@gmail.com>
2025-11-23 12:52:37 +0200

    spring-boot-rest-crud-students-list-refactored

---
+++ 04-spring-boot-rest-crud/03-spring-boot-rest-crud-students-list-refactored/src/main/java/com/luv2demo/demo/rest/StudentRestController.java
@@ -0,0 +1,35 @@
+package com.luv2demo.demo.rest;
+
+import com.luv2demo.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> theStudents;
+
+    // define @PostConstruct to load the student data ... only once!
+    @PostConstruct
+    public void loadDate(){
+        theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("Amr","Kahla"));
+        theStudents.add(new Student("Abed","Kahla"));
+        theStudents.add(new Student("Eman","Abdelrahman"));
+    }
+
+    // define endpoint for "/students" - return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+
+
+        return theStudents;
+    }
+}
