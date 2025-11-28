9690d14bb80a0f89252c55900538024cd1b848d1
Amr Kahla <amrkahla6@gmail.com>
2025-11-28 13:53:05 +0200

    spring-boot-rest-crud-employee-rest-controller-update-employee

---
+++ 04-spring-boot-rest-crud/13-spring-boot-rest-crud-employee-rest-controller-update-employee/src/main/java/com/luv2code/springboot/curddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,42 @@
+package com.luv2code.springboot.curddemo.service;
+
+import com.luv2code.springboot.curddemo.dao.EmployeeDao;
+import com.luv2code.springboot.curddemo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    private EmployeeDao employeeDao;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeDao theEmployeeDao) {
+        employeeDao = theEmployeeDao;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeDao.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return employeeDao.findById(theId);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee theEmployee) {
+        return employeeDao.save(theEmployee);
+    }
+    
+    @Transactional
+    @Override
+    public void deleteById(int theId) {
+        employeeDao.deleteEmployeeById(theId);
+    }
+}
