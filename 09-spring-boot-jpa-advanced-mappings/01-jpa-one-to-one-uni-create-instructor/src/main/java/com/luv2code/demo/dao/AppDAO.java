3a8fd475f2f3e75c0fae2c84b2f0ab4bce1ce083
Amr Kahla <amrkahla6@gmail.com>
2026-01-16 17:28:48 +0200

    create AppDAOImpl

---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni-create-instructor/src/main/java/com/luv2code/demo/dao/AppDAO.java
@@ -0,0 +1,7 @@
+package com.luv2code.demo.dao;
+
+import com.luv2code.demo.entity.Instructor;
+
+public interface AppDAO {
+    void save(Instructor theInstructor);
+}
---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni-create-instructor/src/main/java/com/luv2code/demo/dao/AppDAOImpl.java
@@ -0,0 +1,24 @@
+package com.luv2code.demo.dao;
+
+import com.luv2code.demo.entity.Instructor;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class AppDAOImpl implements AppDAO{
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Transactional
+    @Override
+    public void save(Instructor theInstructor) {
+        entityManager.persist(theInstructor);
+    }
+}
