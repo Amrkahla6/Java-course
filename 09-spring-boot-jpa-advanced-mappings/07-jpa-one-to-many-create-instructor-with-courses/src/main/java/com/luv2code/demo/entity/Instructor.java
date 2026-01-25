0828c40c756cedc10da81735351e2eede9d19046
Amr Kahla <amrkahla6@gmail.com>
2026-01-25 11:35:53 +0200

    jpa-one-to-many-create-instructor-with-course-update-Instructor-class

--- 09-spring-boot-jpa-advanced-mappings/07-jpa-one-to-many-create-instructor-with-courses/src/main/java/com/luv2code/demo/entity/Instructor.java
+++ 09-spring-boot-jpa-advanced-mappings/07-jpa-one-to-many-create-instructor-with-courses/src/main/java/com/luv2code/demo/entity/Instructor.java
@@ -2,6 +2,9 @@ package com.luv2code.demo.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "instructor")
 public class Instructor {
@@ -24,6 +27,10 @@ public class Instructor {
     @JoinColumn(name = "instructor_detail_id")
     private InstructorDetail  instructorDetail;
 
+    @OneToMany(mappedBy = "instructor",
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+                    CascadeType.DETACH, CascadeType.REFRESH})
+    private List<Course> courses;
 
     public Instructor() {
     }
@@ -74,6 +81,14 @@ public class Instructor {
         this.instructorDetail = instructorDetail;
     }
 
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
     @Override
     public String toString() {
         return "Instructor{" +
@@ -83,5 +98,15 @@ public class Instructor {
                 ", email='" + email + '\'' +
                 ", instructorDetail=" + instructorDetail +
                 '}';
+    }
+
+    // add convenience methods for bi-directional relationship
+    public void add(Course tempCourse) {
+        if (courses == null) {
+            courses = new ArrayList<>();
+        }
+
+        courses.add(tempCourse);
+        tempCourse.setTheInstructor(this);
     }
 }
