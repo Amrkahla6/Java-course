495ce319fb9d396b04b075937d14a2a05621093c
Amr Kahla <amrkahla6@gmail.com>
2026-02-14 17:48:12 +0200

    spring-boot-aop-pointcut-expressions-match-any-return-type

--- 10-spring-boot-aop/05-spring-boot-aop-pointcut-expressions-match-any-return-type/src/main/java/com/luv2code/demo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/05-spring-boot-aop-pointcut-expressions-match-any-return-type/src/main/java/com/luv2code/demo/aspect/MyDemoLoggingAspect.java
@@ -13,7 +13,7 @@ public class MyDemoLoggingAspect {
     // let's start with an @Before advice
 
     //@Before("execution(public void add*())")
-    @Before("execution(void add*())")
+    @Before("execution(* add*())")
     public void beforeAddAccountAdvice(){
         System.out.println("\n=====>>> Executing @Before advice on updateAccount()");
     }
