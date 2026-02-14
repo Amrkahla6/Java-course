5c86380c4dd8640c937781632b84385bb7ce8a2f
Amr Kahla <amrkahla6@gmail.com>
2026-02-14 17:39:49 +0200

    spring-boot-aop-pointcut-expressions-match-any-addStar-method

---
+++ 10-spring-boot-aop/04-spring-boot-aop-pointcut-expressions-match-any-addStar-method/src/main/java/com/luv2code/demo/dao/MembershipDaoImpl.java
@@ -0,0 +1,12 @@
+package com.luv2code.demo.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MembershipDaoImpl implements MembershiptDao{
+
+    @Override
+    public void addSillyMember() {
+        System.out.println(getClass() + ": DOING MY DB WORK: ADDING A MEMBERSHIP ACCOUNT");
+    }
+}
