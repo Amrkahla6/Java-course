2861ef124cef13bc034a616f4786407b8aefdd1d
Amr Kahla <amrkahla6@gmail.com>
2026-02-14 17:31:17 +0200

    spring-boot-aop-pointcut-expressions-match-any-add-account

---
+++ 10-spring-boot-aop/02-spring-boot-aop-pointcut-expressions-match-any-add-account/src/main/java/com/luv2code/demo/AopdemoApplication.java
@@ -0,0 +1,29 @@
+package com.luv2code.demo;
+
+import com.luv2code.demo.dao.AccountDao;
+import com.luv2code.demo.dao.MembershiptDao;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AopdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopdemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDao theAccountDao, MembershiptDao theMembershipDao) {
+		return runner -> {
+			demoTheBeforeAdvice(theAccountDao,theMembershipDao);
+		};
+	}
+
+	private void demoTheBeforeAdvice(AccountDao theAccountDao, MembershiptDao theMembershipDao) {
+		theAccountDao.addAccount();
+		theMembershipDao.addAccount();
+	}
+
+}
