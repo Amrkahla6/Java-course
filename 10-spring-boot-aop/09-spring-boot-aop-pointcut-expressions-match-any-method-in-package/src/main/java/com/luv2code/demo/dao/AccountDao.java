223df4de8970a2be4b28ae8b7afdf0438f5c9774
Amr Kahla <amrkahla6@gmail.com>
2026-02-15 14:44:44 +0200

    spring-boot-aop-pointcut-expressions-match-any-method-in-package

---
+++ 10-spring-boot-aop/09-spring-boot-aop-pointcut-expressions-match-any-method-in-package/src/main/java/com/luv2code/demo/dao/AccountDao.java
@@ -0,0 +1,8 @@
+package com.luv2code.demo.dao;
+
+import com.luv2code.demo.Account;
+
+public interface AccountDao {
+    void addAccount(Account theAccount,boolean vipFlag);
+    boolean doWork();
+}
